2402d3933319400241e43a2b0dc048b3c8c109d4
abner-l <liuchao0206@gmail.com>
2014-06-19 00:57:49 +0800

    fengzhuang

--- src/com/yezhizhen/contactoperation/IdSaveDbHelper.java
+++ src/com/yezhizhen/contactoperation/IdSaveDbHelper.java
@@ -1,13 +1,29 @@
 package com.yezhizhen.contactoperation;
 
+import java.io.ByteArrayOutputStream;
+
 import android.R.anim;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.RawContacts;
+import android.provider.ContactsContract.CommonDataKinds.Photo;
+import android.provider.ContactsContract.CommonDataKinds.StructuredName;
+import android.provider.ContactsContract.Contacts.Data;
+import android.util.Log;
+import android.widget.Toast;
 
 public class IdSaveDbHelper extends SQLiteOpenHelper {
-	
+	Bitmap  avatarBM;
+	long rawContactID;
+	ContentValues values;
 	
 	private IdSaveDbHelper(Context context, String name, CursorFactory factory,
 			int version) {
@@ -37,5 +53,90 @@ public class IdSaveDbHelper extends SQLiteOpenHelper {
 		idSaveDbHelper.close();
 		
 	}
+	//定义一个方法 保存联系人信息
+		public void saveContactInfo (ContentResolver contentResolver ,long rawContactID , ContentValues values , String[] info){
+			
+			// 1.保存联系人的姓名 ok
+			values.put(Data.RAW_CONTACT_ID, rawContactID);
+			values.put(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE);
+			values.put(StructuredName.GIVEN_NAME, info[0]);
+			contentResolver.insert(
+					android.provider.ContactsContract.Data.CONTENT_URI, values);
+			values.clear();
+			// 3.保存联系人的昵称 ok
+			values.put(Data.RAW_CONTACT_ID, rawContactID);
+			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Nickname.CONTENT_ITEM_TYPE);
+			values.put(Data.DATA1, info[1]);
+			contentResolver.insert(
+					android.provider.ContactsContract.Data.CONTENT_URI, values);
+			values.clear();
+			// 4.保存联系人的电话 ok
+			values.put(Data.RAW_CONTACT_ID, rawContactID);
+			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
+			values.put(Data.DATA1, info[2]);
+			contentResolver.insert(
+					android.provider.ContactsContract.Data.CONTENT_URI, values);
+			values.clear();
+			// 5.保存联系人的email ok
+			values.put(Data.RAW_CONTACT_ID, rawContactID);
+			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE);
+			values.put(Data.DATA1, info[3]);
+			contentResolver.insert(
+					android.provider.ContactsContract.Data.CONTENT_URI, values);
+			values.clear();
+			// 6.保存联系人的网址 ok
+			values.put(Data.RAW_CONTACT_ID, rawContactID);
+			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE);
+			values.put(Data.DATA1, info[4]);
+			contentResolver.insert(
+					android.provider.ContactsContract.Data.CONTENT_URI, values);
+			values.clear();
+			// 7.保存联系人的地址 ok
+			values.put(Data.RAW_CONTACT_ID, rawContactID);
+			values.put(Data.MIMETYPE,
+					"vnd.android.cursor.item/postal-address_v2");
+			values.put(Data.DATA1, info[5]);
+			contentResolver.insert(
+					android.provider.ContactsContract.Data.CONTENT_URI, values);
+			values.clear();
+			// 8.保存联系人的组织信息 ok
+			values.put(Data.RAW_CONTACT_ID, rawContactID);
+			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE);
+			values.put(Data.DATA1, info[6]);
+			contentResolver.insert(
+					android.provider.ContactsContract.Data.CONTENT_URI, values);
+			values.clear();
+			// 9.保存联系人的event
+			values.put(Data.RAW_CONTACT_ID, rawContactID);
+			values.put(Data.MIMETYPE,
+					ContactsContract.CommonDataKinds.Event.CONTENT_ITEM_TYPE);
+			values.put(Data.DATA1, info[7]);
+			contentResolver.insert(
+					android.provider.ContactsContract.Data.CONTENT_URI, values);
+			values.clear();
+			// 10.保存联系人的即时信息
+			values.put(Data.RAW_CONTACT_ID, rawContactID);
+			values.put(Data.MIMETYPE,
+					ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE);
+			values.put(Data.DATA1, info[8]);
+			contentResolver.insert(
+					android.provider.ContactsContract.Data.CONTENT_URI, values);
+			values.clear();
+		}
+		// 定义一个方法存储联系人到头像到数据库
+		public void updateAvatar(long rawContactID ,ContentValues values,ContentResolver contentResolver,Bitmap avatarBM){
+			final ByteArrayOutputStream os = new ByteArrayOutputStream();
+			 avatarBM.compress(Bitmap.CompressFormat.PNG, 100, os); 
+			byte[] avatar =os.toByteArray(); 
+			Log.e("aa", rawContactID+"");
+			values.put(Data.RAW_CONTACT_ID, rawContactID);
+			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE);
+			values.put(Photo.PHOTO, avatar);
+			contentResolver.insert(android.provider.ContactsContract.Data.CONTENT_URI, values);
+			values.clear();
+		}
 
-}
+	}
+
+
+
--- src/com/yezhizhen/contactoperation/MainActivity.java
+++ src/com/yezhizhen/contactoperation/MainActivity.java
@@ -44,182 +44,79 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		saveContact();
+		operateContact();
 
 	}
 
-	// 保存联系人的方法
-	public void saveContact() {
-		nameEditText = (EditText) findViewById(R.id.et_name);
-		nicknameEditText = (EditText) findViewById(R.id.et_nickname);
-		phoneEditText = (EditText) findViewById(R.id.et_phone);
-		emailEditText = (EditText) findViewById(R.id.et_email);
-		websiteEditText = (EditText) findViewById(R.id.et_website);
-		adressEditText = (EditText) findViewById(R.id.et_adress);
-		orginfoEditText = (EditText) findViewById(R.id.et_orginfo);
-		eventEditText = (EditText) findViewById(R.id.et_event);
-		timelyinfoEditText = (EditText) findViewById(R.id.et_timelyinfo);
+	//操作联系人的方法
+		public void operateContact() {
+			nameEditText = (EditText) findViewById(R.id.et_name);
+			nicknameEditText = (EditText) findViewById(R.id.et_nickname);
+			phoneEditText = (EditText) findViewById(R.id.et_phone);
+			emailEditText = (EditText) findViewById(R.id.et_email);
+			websiteEditText = (EditText) findViewById(R.id.et_website);
+			adressEditText = (EditText) findViewById(R.id.et_adress);
+			orginfoEditText = (EditText) findViewById(R.id.et_orginfo);
+			eventEditText = (EditText) findViewById(R.id.et_event);
+			timelyinfoEditText = (EditText) findViewById(R.id.et_timelyinfo);
 
-		photoImageView = (ImageView) findViewById(R.id.iv_photo);
-		Button setPhotoButton = (Button) findViewById(R.id.btn_set_photo);
-		Button deleteButton = (Button) findViewById(R.id.btn_delete);
-		Button saveButton = (Button) findViewById(R.id.btn_save);
+			photoImageView = (ImageView) findViewById(R.id.iv_photo);
+			Button setPhotoButton = (Button) findViewById(R.id.btn_set_photo);
+			Button deleteButton = (Button) findViewById(R.id.btn_delete);
+			Button saveButton = (Button) findViewById(R.id.btn_save);
 
-		saveButton.setOnClickListener(this);
-		deleteButton.setOnClickListener(this);
-		setPhotoButton.setOnClickListener(this);
+			saveButton.setOnClickListener(this);
+			deleteButton.setOnClickListener(this);
+			setPhotoButton.setOnClickListener(this);
 
-	}
-
-	@Override
-	public void onClick(View view) {
-		
-		switch (view.getId()) {
-		case R.id.btn_save:
-			// 保存联系人
-			values = new ContentValues();
-			Uri rawContactUri = getContentResolver().insert(
-					RawContacts.CONTENT_URI, values);
-			 rawContactID = ContentUris.parseId(rawContactUri);
-			values.clear();
-			// 1.保存联系人的姓名 ok
-			String name = nameEditText.getText().toString().trim();
-			values.put(Data.RAW_CONTACT_ID, rawContactID);
-			values.put(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE);
-			values.put(StructuredName.GIVEN_NAME, name);
-			getContentResolver().insert(
-					android.provider.ContactsContract.Data.CONTENT_URI, values);
-			values.clear();
-			// 2.保存联系人的 id 将联系人的contact_id存储到数据库
-			IdSaveDbHelper idSaveDbHelper = IdSaveDbHelper.getHelper(MainActivity.this);
-			idSaveDbHelper.saveContactId(rawContactID, idSaveDbHelper);
-//			IdSaveDbHelper idSaveDbHelper = new IdSaveDbHelper(this,
-//					"contacts_id.db", null, 1);
-//			idSaveDbHelper.getWritableDatabase().execSQL("insert into contacts_id values(null , ?)", new Object[]{rawContactID});
-//			idSaveDbHelper.close();
-			// 3.保存联系人的昵称 ok
-			String nickname = nicknameEditText.getText().toString().trim();
-			values.put(Data.RAW_CONTACT_ID, rawContactID);
-			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Nickname.CONTENT_ITEM_TYPE);
-			values.put(Data.DATA1, nickname);
-			getContentResolver().insert(
-					android.provider.ContactsContract.Data.CONTENT_URI, values);
-			values.clear();
-			// 4.保存联系人的电话 ok
-			String phone = phoneEditText.getText().toString().trim();
-			values.put(Data.RAW_CONTACT_ID, rawContactID);
-			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
-			values.put(Data.DATA1, phone);
-			getContentResolver().insert(
-					android.provider.ContactsContract.Data.CONTENT_URI, values);
-			values.clear();
-			// 5.保存联系人的email ok
-			String email = emailEditText.getText().toString().trim();
-			values.put(Data.RAW_CONTACT_ID, rawContactID);
-			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE);
-			values.put(Data.DATA1, email);
-			getContentResolver().insert(
-					android.provider.ContactsContract.Data.CONTENT_URI, values);
-			values.clear();
-			// 6.保存联系人的网址 ok
-			String website = websiteEditText.getText().toString().trim();
-			values.put(Data.RAW_CONTACT_ID, rawContactID);
-			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE);
-			values.put(Data.DATA1, website);
-			getContentResolver().insert(
-					android.provider.ContactsContract.Data.CONTENT_URI, values);
-			values.clear();
-			// 7.保存联系人的地址 ok
-			String adress = adressEditText.getText().toString().trim();
-			values.put(Data.RAW_CONTACT_ID, rawContactID);
-			values.put(Data.MIMETYPE,
-					"vnd.android.cursor.item/postal-address_v2");
-			values.put(Data.DATA1, adress);
-			getContentResolver().insert(
-					android.provider.ContactsContract.Data.CONTENT_URI, values);
-			values.clear();
-			// 8.保存联系人的组织信息 ok
-			String orginfo = orginfoEditText.getText().toString().trim();
-			values.put(Data.RAW_CONTACT_ID, rawContactID);
-			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE);
-			values.put(Data.DATA1, orginfo);
-			getContentResolver().insert(
-					android.provider.ContactsContract.Data.CONTENT_URI, values);
-			values.clear();
-			// 9.保存联系人的event
-			String event = eventEditText.getText().toString().trim();
-			values.put(Data.RAW_CONTACT_ID, rawContactID);
-			values.put(Data.MIMETYPE,
-					ContactsContract.CommonDataKinds.Event.CONTENT_ITEM_TYPE);
-			values.put(Data.DATA1, event);
-			getContentResolver().insert(
-					android.provider.ContactsContract.Data.CONTENT_URI, values);
-			values.clear();
-			// 10.保存联系人的即时信息
-			String timelyinfo = timelyinfoEditText.getText().toString().trim();
-			values.put(Data.RAW_CONTACT_ID, rawContactID);
-			values.put(Data.MIMETYPE,
-					ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE);
-			values.put(Data.DATA1, timelyinfo);
-			getContentResolver().insert(
-					android.provider.ContactsContract.Data.CONTENT_URI, values);
-			values.clear();
-			Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
-			updateAvatar(rawContactID);
-			
-			break;
-		case R.id.btn_delete:
-			// 删除联系人
-			break;
-		case R.id.btn_set_photo:
-			// 设置头像
-			Random photoName = new Random();
-			int i =	photoName.nextInt(10);
-
-			photoImageView.setBackgroundResource(R.drawable.photo1+i);
-
-			Bitmap	photoBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.photo1+i);
-		
-			values.put(Data.RAW_CONTACT_ID, rawContactID);
-			values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE);
-			values.put(Data.DATA1, photoBitmap.toString().getBytes());
-			
-			values.clear();
-			Toast.makeText(this, "头像设置成功", Toast.LENGTH_SHORT).show();
-
-			avatarBM = BitmapFactory.decodeResource(getResources(), R.drawable.photo1+i);
-//			File photoFile = new File(getApplication().getFilesDir(), "photo.png");
-//			try {
-//				OutputStream photoOuputStream = new FileOutputStream(photoFile);
-//				try {
-//					photoOuputStream.write(photoBitmap.toString().getBytes());
-//				} catch (IOException e) {
-//					e.printStackTrace();
-//				}
-//			} catch (FileNotFoundException e) {
-//				e.printStackTrace();
-//			}
-			photoImageView.setImageBitmap(avatarBM);
-
-
-			break;
-		default:
-			break;
 		}
 
-	}
+		@Override
+		public void onClick(View view) {
+			
+			switch (view.getId()) {
+			case R.id.btn_save:
+				// 保存联系人
+				values = new ContentValues();
+				Uri rawContactUri = getContentResolver().insert(
+						RawContacts.CONTENT_URI, values);
+				rawContactID = ContentUris.parseId(rawContactUri);
+				values.clear();
+				
+				// 2.保存联系人的 id 将联系人的contact_id存储到数据库
+				IdSaveDbHelper idSaveDbHelper = IdSaveDbHelper.getHelper(MainActivity.this);
+				idSaveDbHelper.saveContactId(rawContactID, idSaveDbHelper);
+				
+				String name = nameEditText.getText().toString().trim();
+				String nickname = nicknameEditText.getText().toString().trim();
+				String phone = phoneEditText.getText().toString().trim();
+				String email = emailEditText.getText().toString().trim();
+				String website = websiteEditText.getText().toString().trim();
+				String adress = adressEditText.getText().toString().trim();
+				String orginfo = orginfoEditText.getText().toString().trim();
+				String event = eventEditText.getText().toString().trim();
+				String timelyinfo = timelyinfoEditText.getText().toString().trim();
+				String[]info = new String[]{name,nickname,phone,email,website,adress,orginfo,event,timelyinfo};
+				idSaveDbHelper.saveContactInfo(getContentResolver(), rawContactID,values,info);
+				//保存联系人头像
+				idSaveDbHelper.updateAvatar(rawContactID,values,getContentResolver(), avatarBM);
+				Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.btn_delete:
+				// 删除联系人
+				break;
+			case R.id.btn_set_photo:
+				// 设置头像
+				Random photoName = new Random();
+				int i =	photoName.nextInt(10);
+				avatarBM = BitmapFactory.decodeResource(getResources(), R.drawable.photo1+i);
+				photoImageView.setImageBitmap(avatarBM);
+				break;
+			default:
+				break;
+			}
 
-	public void updateAvatar(long contactID){
-		final ByteArrayOutputStream os = new ByteArrayOutputStream();
-		avatarBM.compress(Bitmap.CompressFormat.PNG, 100, os); 
-		byte[] avatar =os.toByteArray(); 
-		Log.e("aa", rawContactID+"");
-		values.put(Data.RAW_CONTACT_ID, rawContactID);
-		values.put(Data.MIMETYPE, ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE);
-		values.put(Photo.PHOTO, avatar);
+		}
+
 		
-		getContentResolver().insert(android.provider.ContactsContract.Data.CONTENT_URI, values);
-		values.clear();
-		Toast.makeText(this, "头像设置成功", Toast.LENGTH_SHORT).show();
 	}
-}
